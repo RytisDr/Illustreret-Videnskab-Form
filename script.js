86c9a47c0dd36342bf4e638011d1006a057cb720
RytisDr <r.drazdauskas@gmail.com>
2018-11-01 19:10:00 +0100

    Creates functions to show morw options and improves radio button checking.

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", init);
-
-function init() {}
-
+function init() {
+  let radioSelections = document.querySelectorAll(".selection");
+  let morePaymentOpt = document.querySelector("#moreOptions");
+  radioSelections.forEach(e =>
+    e.addEventListener("click", function() {
+      e.querySelector("input").checked = true;
+    })
+  );
+  morePaymentOpt.addEventListener("click", function() {
+    showMoreOpt(morePaymentOpt);
+  });
+}
+function showMoreOpt(button) {
+  button.style.opacity = "0";
+  button.addEventListener("transitionend", function() {
+    button.style.display = "none";
+    document
+      .querySelectorAll(".selection")
+      .forEach(e => e.classList.remove("hidden"));
+  });
+}
 function switchPages() {}
