dbac7622ad0897e8d5a0d209e40ca3c39785def8
elahe ar <elahe@Brugers-MacBook-Pro-2.local>
2018-11-01 12:10:24 +0100

    landing page and selecting subscription

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {}
 
+const orderButton = document.querySelector("#order");
+const checkoutPage = document.getElementById("magazineNr");
+const landingPage = document.getElementById("landingpage");
+const backButton = document.getElementById("back");
+
+orderButton.addEventListener("click", switchtocheckout);
+function switchtocheckout() {
+  checkoutPage.style.marginLeft = "0";
+  landingPage.classList.add("hidden");
+  checkoutPage.classList.remove("hidden");
+}
+backButton.addEventListener("click", back);
+function back() {
+  checkoutPage.classList.add("hidden");
+  landingPage.classList.remove("hidden");
+}
 function switchPages() {}
